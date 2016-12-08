7cbd7a179f54dd4557d4102b3efc5797cf3e5b2c
Dmitriy Babkov <dsbabkov@gmail.com>
2016-12-08 16:48:42 +0300

    Добавлен класс точки

---
+++ lab9/Point.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <iosfwd>
+
+class Point
+{
+public:
+    Point();
+    Point(double x, double y);
+
+    double x() const;
+    void setX(double x);
+
+    double y() const;
+    void setY(double y);
+
+    Point &operator -=(const Point &other);
+    Point &operator +=(const Point &other);
+    Point operator -(const Point &other) const;
+    Point operator +(const Point &other) const;
+
+    bool operator == (const Point &other) const;
+    bool operator != (const Point &other) const;
+
+    double vectorLength() const;
+
+private:
+    double x_;
+    double y_;
+};
+
+std::ostream &operator << (std::ostream &os, const Point &point);
