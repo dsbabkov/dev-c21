33a0bbc4c2531f7105507b682d865229ddb47f2d
Dmitriy Babkov <dsbabkov@gmail.com>
2016-12-09 13:18:45 +0300

    Добавлен класс точки

---
+++ lab10/Point.cpp
@@ -0,0 +1,86 @@
+#include "Point.h"
+#include <cmath>
+#include <iostream>
+
+Point::Point()
+    : x_{}
+    , y_{}
+{
+}
+
+Point::Point(double x, double y)
+    : x_{x}
+    , y_{y}
+{
+}
+
+double Point::x() const
+{
+    return x_;
+}
+
+void Point::setX(double x)
+{
+    x_ = x;
+}
+
+double Point::y() const
+{
+    return y_;
+}
+
+void Point::setY(double y)
+{
+    y_ = y;
+}
+
+Point &Point::operator -=(const Point &other)
+{
+    x_ -= other.x_;
+    y_ -= other.y_;
+
+    return *this;
+}
+
+Point &Point::operator +=(const Point &other)
+{
+    x_ += other.x_;
+    y_ += other.y_;
+
+    return *this;
+}
+
+Point Point::operator -(const Point &other) const
+{
+    return {x_ - other.x_,
+                y_ - other.y_};
+}
+
+Point Point::operator +(const Point &other) const
+{
+        return {x_ + other.x_,
+                    y_ + other.y_};
+}
+
+bool Point::operator ==(const Point &other) const
+{
+    return x_ == other.x_ &&
+            y_ == other.y_;
+}
+
+bool Point::operator < (const Point &other) const{
+    return x_ < other.x_ ||
+            y_ < other.y_;
+}
+
+double Point::vectorLength() const
+{
+    return std::sqrt(pow(x_, 2) + pow(y_, 2));
+}
+
+
+std::ostream &operator <<(std::ostream &os, const Point &point)
+{
+    os << '{' << point.x() << "; " << point.y() << '}';
+    return os;
+}
